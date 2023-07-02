60215befd8be64b0228897f9f7e4849812934a2a
Hanck1215 <st11025212@gmail.com>
2023-07-03 02:29:17 +0800

    記住原先的commit ID

--- Person.py
+++ Person.py
@@ -2,3 +2,6 @@ class Person() :
     age = 0
     name = ""
     height = 0
+
+
+record = ["c9eb4ad (HEAD -> Person) 新增height屬性", "0cc3cd8 新增name屬性", "223fe14 新增age屬性", "7da2fc7 新增Person類別", "b9f9608 (origin/main, origin/HEAD, main) Initial commit"]
